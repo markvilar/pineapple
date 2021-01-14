ea634465fbe30a3b1ecdaebae218ac42b37eff97
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2021-01-14 07:48:38 +0100

    Add framebuffer and viewport to ControlLayer.

--- src/ControlLayer.cpp
+++ src/ControlLayer.cpp
@@ -13,8 +13,6 @@ ControlLayer::~ControlLayer()
 
 void ControlLayer::OnAttach()
 {
-	Sennet::Synchronizer::Get().BeginSession("Synchronization");
-
 	// Set up client.
 	m_Client = CreateRef<Client>();
 
@@ -24,6 +22,15 @@ void ControlLayer::OnAttach()
 	m_RecordingParametersPanel.SetClient(m_Client);
 	m_RuntimeParametersPanel.SetClient(m_Client);
 	m_SensorControllerPanel.SetClient(m_Client);
+
+	// Set up framebuffer.
+	FramebufferSpecification fbSpec;
+	fbSpec.Width = 1280;
+	fbSpec.Height = 720;
+	m_Framebuffer = Framebuffer::Create(fbSpec);
+
+	Renderer2D::Init();
+
 }
 
 void ControlLayer::OnDetach()
@@ -38,8 +45,19 @@ void ControlLayer::OnUpdate(Timestep ts)
 		OnMessage(message);
 	}
 
-	m_CameraController.OnUpdate(ts);
+	FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+	if (m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f &&
+		(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+	{
+		m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, 
+			(uint32_t)m_ViewportSize.y);
+		m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+	}
 
+	if (m_ViewportFocused)
+		m_CameraController.OnUpdate(ts);
+
+	m_Framebuffer->Bind();
 	RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1.0f});
 	RenderCommand::Clear();
 
@@ -61,14 +79,63 @@ void ControlLayer::OnUpdate(Timestep ts)
 	}
 
 	Renderer2D::EndScene();
+	m_Framebuffer->Unbind();
 }
 
 void ControlLayer::OnImGuiRender()
 {
-	static bool show = true;
-	ImGui::ShowDemoWindow(&show);
+	static bool dockspaceOpen = true;
+	static bool optionFullscreenPersistant = true;
+	bool optionFullscreen = optionFullscreenPersistant;
+	static ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_None;
 
-	ImGui::SetNextWindowSize(ImVec2(420,600));
+	ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar 
+		| ImGuiWindowFlags_NoDocking;
+	if (optionFullscreen)
+	{
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->GetWorkPos());
+		ImGui::SetNextWindowSize(viewport->GetWorkSize());
+		ImGui::SetNextWindowViewport(viewport->ID);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+		windowFlags |= ImGuiWindowFlags_NoTitleBar 
+			| ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize 
+			| ImGuiWindowFlags_NoMove;
+		windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus 
+			| ImGuiWindowFlags_NoNavFocus;
+	}
+
+	if (dockspaceFlags & ImGuiDockNodeFlags_PassthruCentralNode)
+		windowFlags |= ImGuiWindowFlags_NoBackground;
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::Begin("DockSpace Demo", &dockspaceOpen, windowFlags);
+	ImGui::PopStyleVar();
+	
+	if (optionFullscreen)
+		ImGui::PopStyleVar(2);
+
+	// DockSpace
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+	{
+		ImGuiID dockspaceID = ImGui::GetID("MyDockSpace");
+		ImGui::DockSpace(dockspaceID, ImVec2(0.0f, 0.0f), dockspaceFlags);
+	}
+
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Exit"))
+				Application::Get().Close();
+			ImGui::EndMenu();
+		}
+		ImGui::EndMenuBar();
+	}
+
+	//ImGui::SetNextWindowSize(ImVec2(420,600));
 	if (ImGui::Begin("Control Layer"))
 	{
 		m_ClientPanel.OnImGuiRender();
@@ -79,6 +146,26 @@ void ControlLayer::OnImGuiRender()
 		m_RuntimeParametersPanel.OnImGuiRender();
 		ImGui::End();
 	}
+
+	// Viewport window.
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
+	ImGui::Begin("Viewport");
+
+	m_ViewportFocused = ImGui::IsWindowFocused();
+	m_ViewportHovered = ImGui::IsWindowHovered();
+	Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused 
+		|| !m_ViewportHovered);
+
+	ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
+	m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+
+	uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
+	ImGui::Image((void*)textureID, ImVec2{ viewportPanelSize.x, 
+		viewportPanelSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+	ImGui::End();
+	ImGui::PopStyleVar();
+
+	ImGui::End();
 }
 
 void ControlLayer::OnEvent(Event& e)
